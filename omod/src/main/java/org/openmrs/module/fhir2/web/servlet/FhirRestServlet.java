a4c5f27e4702939b5a5be43406a2dba0f1849259
Ian <ian_bacher@brown.edu>
2021-01-11 09:42:22 -0500

    FM2-163: Add support for FHIR resources not in the FHIR module

--- omod/src/main/java/org/openmrs/module/fhir2/web/servlet/FhirRestServlet.java
+++ omod/src/main/java/org/openmrs/module/fhir2/web/servlet/FhirRestServlet.java
@@ -9,6 +9,8 @@
  */
 package org.openmrs.module.fhir2.web.servlet;
 
+import static org.openmrs.module.fhir2.FhirConstants.FHIR2_MODULE_ID;
+
 import java.util.Collection;
 
 import ca.uhn.fhir.context.FhirContext;
@@ -21,15 +23,19 @@ import ca.uhn.fhir.rest.server.interceptor.LoggingInterceptor;
 import lombok.AccessLevel;
 import lombok.Setter;
 import org.apache.commons.lang3.math.NumberUtils;
+import org.openmrs.module.ModuleFactory;
+import org.openmrs.module.fhir2.FhirActivator;
 import org.openmrs.module.fhir2.FhirConstants;
 import org.openmrs.module.fhir2.api.FhirGlobalPropertyService;
 import org.openmrs.module.fhir2.narrative.OpenMRSThymeleafNarrativeGenerator;
 import org.openmrs.module.fhir2.web.util.NarrativeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.MessageSource;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 @Component
 @Setter(AccessLevel.PUBLIC)
@@ -44,13 +50,30 @@ public class FhirRestServlet extends RestfulServer {
 	@Qualifier("hapiLoggingInterceptor")
 	private LoggingInterceptor loggingInterceptor;
 	
+	private ConfigurableApplicationContext ctx;
+	
+	@Autowired
+	@Qualifier("messageSourceService")
 	private MessageSource messageSource;
 	
 	@Override
 	protected void initialize() {
-		// ensure properties for this class are properly injected
+		// we need to load the application context for the FHIR2 module
 		if (globalPropertyService == null) {
-			SpringBeanAutowiringSupport.processInjectionBasedOnServletContext(this, getServletContext());
+			// get the activator which contains our ApplicationContext
+			FhirActivator activator = (FhirActivator) ModuleFactory.getModuleById(FHIR2_MODULE_ID).getModuleActivator();
+			ctx = activator.getApplicationContext();
+			
+			// reload ResourceProviders whenever the application context is refreshed
+			ctx.addApplicationListener(e -> {
+				if (e instanceof ContextRefreshedEvent) {
+					unregisterProviders(getResourceProviders());
+					registerProviders(ctx.getBean(getResourceProviderListName()));
+				}
+			});
+			
+			// ensure properties for this class are properly injected
+			autoInject();
 		}
 		
 		int defaultPageSize = NumberUtils
@@ -82,6 +105,16 @@ public class FhirRestServlet extends RestfulServer {
 		        .setNarrativeGenerator(new OpenMRSThymeleafNarrativeGenerator(messageSource, narrativePropertiesFiles));
 	}
 	
+	protected void autoInject() {
+		AutowiredAnnotationBeanPostProcessor bpp = new AutowiredAnnotationBeanPostProcessor();
+		bpp.setBeanFactory(ctx.getAutowireCapableBeanFactory());
+		bpp.processInjection(this);
+	}
+	
+	protected String getResourceProviderListName() {
+		return "fhirResources";
+	}
+	
 	@Override
 	protected String createPoweredByHeaderComponentName() {
 		return FhirConstants.OPENMRS_FHIR_SERVER_NAME;
@@ -111,10 +144,5 @@ public class FhirRestServlet extends RestfulServer {
 	@Autowired
 	public void setServerAddressStrategy(IServerAddressStrategy theServerAddressStrategy) {
 		super.setServerAddressStrategy(theServerAddressStrategy);
-	}
-	
-	@Autowired
-	public void setMessageSource(MessageSource messageSource) {
-		this.messageSource = messageSource;
 	}
 }
